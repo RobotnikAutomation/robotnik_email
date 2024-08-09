b382c7ec2e9cb52822ac6ef29f6d74763272d0c2
RomanRobotnik <rnavarro@robotnik.es>
2024-08-09 13:09:26 +0200

    Apply pylint

--- src/robotnik_email/smtp_manager.py
+++ src/robotnik_email/smtp_manager.py
@@ -6,16 +6,16 @@
 from os.path import basename
 from typing import List, Union
 
-import rospy
-
-from rcomponent.rcomponent import *
-from robotnik_alarms_msgs.srv import SendAlarms, SendAlarmsResponse
-
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 import re
+
+import rospy
+
+from rcomponent.rcomponent import RComponent
+from robotnik_alarms_msgs.srv import SendAlarms, SendAlarmsResponse
 
 
 class SMTPManager(RComponent):
@@ -26,6 +26,16 @@ class SMTPManager(RComponent):
     def __init__(self):
 
         RComponent.__init__(self)
+        self.smtp_server = ''
+        self.smtp_port = 0
+        self.sender = ''
+        self.use_authentication = False
+        self.username = ''
+        self.password = ''
+        self.default_recipients = ''
+        self.time_between_emails = 0
+        self.smtp = None
+        self.send_email_service = None
 
         self.logger = self.initialize_logger()
 
@@ -57,12 +67,21 @@ class SMTPManager(RComponent):
         return 0
 
     def init_state(self):
+        """
+        Initializes the state of the SMTP manager.
 
-        if self.check_recipients(self.default_recipients) == False:
+        This method checks if the default recipients and sender are properly formatted.
+        If any of them is malformed, it logs an error and shuts down the ROS node.
+
+        Returns:
+            The initialization state of the SMTP manager.
+
+        """
+        if self.check_recipients(self.default_recipients) is False:
             self.logger.logerror("Default recipients are malformed", "")
             rospy.signal_shutdown("shutdown")
 
-        if self.check_recipients(self.sender.split()) == False:
+        if self.check_recipients(self.sender.split()) is False:
             self.logger.logerror("Sender is malformed", "")
             rospy.signal_shutdown("shutdown")
 
@@ -74,7 +93,18 @@ class SMTPManager(RComponent):
         return RComponent.ready_state(self)
 
     def emergency_state(self):
+        """
+        Method to handle emergency state.
 
+        This method is responsible for handling the emergency state of the SMTP manager.
+        It performs necessary actions to handle the emergency situation.
+
+        Parameters:
+            self (SMTPManager): The SMTPManager instance.
+
+        Returns:
+            None
+        """
         dummy = 0
         # self.switch_to_state(State.READY_STATE)
 
@@ -85,10 +115,7 @@ class SMTPManager(RComponent):
             0 : if it's performed successfully
             -1: if there's any problem or the component is running
         """
-        try:
-            self.smtp_disconnection()
-        except:
-            pass
+        self.smtp_disconnection()
 
         return RComponent.shutdown(self)
 
@@ -98,21 +125,36 @@ class SMTPManager(RComponent):
         return RComponent.switch_to_state(self, new_state)
 
     def check_recipients(self, recipients):
+        """
+        Checks if the given recipients are valid email addresses.
+
+        Args:
+            recipients (list): A list of email addresses to be checked.
+
+        Returns:
+            bool: True if all recipients are valid email addresses, False otherwise.
+        """
 
         regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
         valid = True
 
         for recipient in recipients:
-
             if not re.search(regex, recipient):
-
                 self.logger.logerror(f"{recipient} is an invalid email", "")
                 valid = False
 
         return valid
 
     def send_email_cb(self, req):
+        """
+        Sends an email based on the given request.
 
+        Args:
+            req: The request object containing the email details.
+
+        Returns:
+            A response object indicating the success or failure of the email sending operation.
+        """
         response = SendAlarmsResponse()
         response.ret.success = False
         response.ret.code = -1
@@ -121,7 +163,7 @@ class SMTPManager(RComponent):
 
             email = self.build_email(req)
 
-            if email != None:
+            if email is not None:
 
                 if self.send_email(email):
 
@@ -144,18 +186,23 @@ class SMTPManager(RComponent):
 
         try:
             self.smtp_disconnection()
-        except:
-            pass
+        except smtplib.SMTPException as e:
+            rospy.logerr(e)
 
         # if (response.ret.success == True) or (response.ret.code == 0):
         #    rospy.loginfo(response.ret.message)
-        if (response.ret.success == False) or (response.ret.code == -1):
+        if (response.ret.success is False) or (response.ret.code == -1):
             self.logger.logerror(response.ret.message, "")
 
         return response
 
     def smtp_connection(self):
+        """
+        Establishes a connection to the SMTP server.
 
+        Returns:
+            bool: True if the connection is successfully established, False otherwise.
+        """
         # self.smtp = smtplib.SMTP(timeout=20)
         self.smtp = smtplib.SMTP(self.smtp_server)
 
@@ -170,7 +217,7 @@ class SMTPManager(RComponent):
 
             success = True
 
-        except Exception as e:
+        except smtplib.SMTPException as e:
             self.logger.logerror(
                 f"smtp_manager::smtp_connection -> Exception: {e}", "")
             success = False
@@ -178,7 +225,18 @@ class SMTPManager(RComponent):
         return success
 
     def build_email(self, email_data):
+        """
+        Builds and returns an email message based on the provided email_data.
 
+        Args:
+            email_data (EmailData): The data object containing information for building the email.
+
+        Returns:
+            email (MIMEMultipart): The constructed email message.
+
+        Raises:
+            None
+        """
         email = MIMEMultipart("alternative")
 
         # Set the Sender
@@ -221,14 +279,22 @@ class SMTPManager(RComponent):
         return email
 
     def send_email(self, email):
+        """
+        Sends an email using the SMTP server.
+
+        Args:
+            email (EmailMessage): The email message to be sent.
+
+        Returns:
+            bool: True if the email was sent successfully, False otherwise.
+        """
 
         try:
-            # print(email["To"])
             self.smtp.sendmail(
                 email["From"], email["To"].split(','), email.as_string())
             success = True
 
-        except Exception as e:
+        except smtplib.SMTPException as e:
             self.logger.logerror(
                 f"smtp_manager::send_email -> Exception: {e}", "")
             success = False
@@ -261,5 +327,15 @@ class SMTPManager(RComponent):
         return attachments, non_attachments
 
     def smtp_disconnection(self):
+        """
+        Disconnects from the SMTP server.
 
-        self.smtp.quit()
+        Returns:
+            bool: True if the disconnection was successful, False otherwise.
+        """
+        try:
+            self.smtp.quit()
+        except smtplib.SMTPException as e:
+            rospy.logerr(e)
+            return False
+        return True
