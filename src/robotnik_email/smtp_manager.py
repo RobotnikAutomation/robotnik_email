8fb3f132965e3081aafcfc011f337cb2a2fc3739
RomanRobotnik <rnavarro@robotnik.es>
2024-08-09 12:38:05 +0200

    Apply PEP8 format

--- src/robotnik_email/smtp_manager.py
+++ src/robotnik_email/smtp_manager.py
@@ -6,7 +6,7 @@
 from os.path import basename
 from typing import List, Union
 
-import rospy 
+import rospy
 
 from rcomponent.rcomponent import *
 from robotnik_alarms_msgs.srv import SendAlarms, SendAlarmsResponse
@@ -17,6 +17,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 import re
 
+
 class SMTPManager(RComponent):
     """
     SMTP server to send messages through ROS
@@ -25,7 +26,7 @@ class SMTPManager(RComponent):
     def __init__(self):
 
         RComponent.__init__(self)
-        
+
         self.logger = self.initialize_logger()
 
     def ros_read_params(self):
@@ -34,13 +35,15 @@ class SMTPManager(RComponent):
 
         self.smtp_server = rospy.get_param('smtp/server', 'smtp.gmail.com')
         self.smtp_port = rospy.get_param('smtp/port', 587)
-        self.sender =  rospy.get_param('smtp/sender', 'sender@domain.com')
-        self.use_authentication = rospy.get_param('smtp/use_authentication', False)
+        self.sender = rospy.get_param('smtp/sender', 'sender@domain.com')
+        self.use_authentication = rospy.get_param(
+            'smtp/use_authentication', False)
         self.username = rospy.get_param('smtp/username', 'username')
         self.password = rospy.get_param('smtp/password', 'password')
-        self.default_recipients = rospy.get_param('smtp/default_recipients', 'recipient@domain.com')
-        self.time_between_emails = rospy.get_param('smtp/time_between_emails', 0)
-
+        self.default_recipients = rospy.get_param(
+            'smtp/default_recipients', 'recipient@domain.com')
+        self.time_between_emails = rospy.get_param(
+            'smtp/time_between_emails', 0)
 
     def ros_setup(self):
         """Creates and inits ROS components"""
@@ -48,12 +51,13 @@ class SMTPManager(RComponent):
         RComponent.ros_setup(self)
 
         # Service
-        self.send_email_service = rospy.Service('robotnik_email/send_email', SendAlarms, self.send_email_cb)
-        
+        self.send_email_service = rospy.Service(
+            'robotnik_email/send_email', SendAlarms, self.send_email_cb)
+
         return 0
 
     def init_state(self):
-        
+
         if self.check_recipients(self.default_recipients) == False:
             self.logger.logerror("Default recipients are malformed", "")
             rospy.signal_shutdown("shutdown")
@@ -70,9 +74,9 @@ class SMTPManager(RComponent):
         return RComponent.ready_state(self)
 
     def emergency_state(self):
-        
+
         dummy = 0
-        #self.switch_to_state(State.READY_STATE)
+        # self.switch_to_state(State.READY_STATE)
 
     def shutdown(self):
         """Shutdowns device
@@ -107,7 +111,6 @@ class SMTPManager(RComponent):
 
         return valid
 
-
     def send_email_cb(self, req):
 
         response = SendAlarmsResponse()
@@ -121,12 +124,13 @@ class SMTPManager(RComponent):
             if email != None:
 
                 if self.send_email(email):
-                    
-                    #response.ret.message = "Email sent from " + email["From"] + " to " + email["To"]
-                    self.logger.loginfo("Email sent from " + email["From"] + " to " + email["To"], "")
+
+                    # response.ret.message = "Email sent from " + email["From"] + " to " + email["To"]
+                    self.logger.loginfo(
+                        "Email sent from " + email["From"] + " to " + email["To"], "")
                     response.ret.success = True
                     response.ret.code = 0
-                    
+
                 else:
 
                     response.ret.message = "The email could not be sent to the recipients, probably due to a mail server failure"
@@ -135,26 +139,26 @@ class SMTPManager(RComponent):
                 response.ret.message = "The email can not be sent because it is malformed"
         else:
 
-            response.ret.message = "Cannot connect to SMTP server " + str(self.smtp_server) + " with port " + str(self.smtp_port)
+            response.ret.message = "Cannot connect to SMTP server " + \
+                str(self.smtp_server) + " with port " + str(self.smtp_port)
 
         try:
             self.smtp_disconnection()
         except:
             pass
 
-        #if (response.ret.success == True) or (response.ret.code == 0):
+        # if (response.ret.success == True) or (response.ret.code == 0):
         #    rospy.loginfo(response.ret.message)
         if (response.ret.success == False) or (response.ret.code == -1):
             self.logger.logerror(response.ret.message, "")
 
         return response
 
-
     def smtp_connection(self):
 
         # self.smtp = smtplib.SMTP(timeout=20)
         self.smtp = smtplib.SMTP(self.smtp_server)
-        
+
         try:
             self.smtp.connect(self.smtp_server, self.smtp_port)
             self.smtp.ehlo()
@@ -167,27 +171,28 @@ class SMTPManager(RComponent):
             success = True
 
         except Exception as e:
-            self.logger.logerror(f"smtp_manager::smtp_connection -> Exception: {e}", "")
+            self.logger.logerror(
+                f"smtp_manager::smtp_connection -> Exception: {e}", "")
             success = False
 
         return success
 
-
     def build_email(self, email_data):
 
         email = MIMEMultipart("alternative")
-  
+
         # Set the Sender
         email["From"] = self.sender
-        
+
         # Set the Subject
         if email_data.status.description == "":
             email["Subject"] = "default"
         else:
-            email["Subject"] =  email_data.status.description
+            email["Subject"] = email_data.status.description
 
         # Set the Attachments
-        attachments, non_attachments = self.get_files_to_upload_as_attachments(email_data.files_to_upload)
+        attachments, non_attachments = self.get_files_to_upload_as_attachments(
+            email_data.files_to_upload)
         for attachment in attachments:
             email.attach(attachment)
 
@@ -200,7 +205,8 @@ class SMTPManager(RComponent):
             for non_attachment in non_attachments:
                 non_attachments_msg += f'<p>  - {non_attachment}</p>'
 
-        email.attach(MIMEText(email_data.status.message + non_attachments_msg, "html"))
+        email.attach(MIMEText(email_data.status.message +
+                     non_attachments_msg, "html"))
 
         # Set the Recipient
         if '' in email_data.recipients or len(email_data.recipients) == 0:
@@ -216,15 +222,17 @@ class SMTPManager(RComponent):
 
     def send_email(self, email):
 
-        try: 
-            #print(email["To"])
-            self.smtp.sendmail(email["From"], email["To"].split(','), email.as_string())
+        try:
+            # print(email["To"])
+            self.smtp.sendmail(
+                email["From"], email["To"].split(','), email.as_string())
             success = True
 
         except Exception as e:
-            self.logger.logerror(f"smtp_manager::send_email -> Exception: {e}", "")
+            self.logger.logerror(
+                f"smtp_manager::send_email -> Exception: {e}", "")
             success = False
-        
+
         return success
 
     def get_files_to_upload_as_attachments(self, files_to_upload: List[str]) -> List[Union[str, MIMEApplication]]:
@@ -238,14 +246,17 @@ class SMTPManager(RComponent):
             attachment_size = round(len(attachment_data) / 1e6, 3)
             total_size += attachment_size
             if total_size < 22.5:
-                attachment = MIMEApplication(attachment_data, Name=basename(file_to_upload))
-                attachment['Content-Disposition'] = f'attachment; filename="{basename(file_to_upload)}"'
+                attachment = MIMEApplication(
+                    attachment_data, Name=basename(file_to_upload))
+                attachment[
+                    'Content-Disposition'] = f'attachment; filename="{basename(file_to_upload)}"'
                 attachments.append(attachment)
             else:
                 non_attachments.append(file_to_upload)
 
         if non_attachments:
-            rospy.logwarn(f'Mail maximum size exceeded. Files {non_attachments} could not be sent as attachments')
+            rospy.logwarn(
+                f'Mail maximum size exceeded. Files {non_attachments} could not be sent as attachments')
 
         return attachments, non_attachments
 
